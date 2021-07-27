c20f8146b9211b30a3e47d930992e405e9f02969
hyleore <badoux.e@gmail.com>
2021-07-27 16:33:06 +0200

    élaboration de main.js + structure simple de niveaux

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const config = {
     width: 1920,
     height: 1080,
     backgroundColor: '#EFDECD',
-    scene : [Title, Main, End, Exercice1],
+    scene : [Title, Main, End, Niveau1, Niveau2],
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
--- scenes/main.js
+++ scenes/main.js
@@ -1,3 +1,7 @@
+let inputText;
+let correctAnswer;
+let textBox;
+
 class Main extends Phaser.Scene {
 
     constructor () {
@@ -6,9 +10,79 @@ class Main extends Phaser.Scene {
         });
     }
 
-    init(data) {}
-    preload () {}
-    create (data)  {}
-    update(time, delta) {}
+init() {}
 
+preload () {
+    this.load.image('biblio2','/assets/library2.jpg');
+    // plugin pour texte input
+    this.load.plugin('rexinputtextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexinputtextplugin.min.js', true);
+    }
+
+create ()  {
+    var biblio2 = this.add.image(0,0,'biblio2').setOrigin(0);
+    biblio2.width = config.width; // je me réfère à la width de config pour adapter à la taille du jeu!
+
+    // zone de texte input
+    inputText = this.add.rexInputText(
+        // x, y, width, height, config
+        1280, 540, 600, 800, {
+        // config de la zone de texte
+
+        // propriétés
+        type: 'textarea',
+        placeholder: 'type here...',
+
+        // style
+        align: 'left',
+        padding: '20px',
+        border: '10px solid',
+        fontFamily: '"Times New Roman", Times, serif',
+        fontSize: '24px',
+        color: '#331A00',
+        backgroundColor: 'white',
+        borderColor: '#DEB083',
+    })
+    .resize(600, 800)
+    .setOrigin(0.5);
+
+    this.scene.launch('Niveau1');
+}
+
+update() {}
+
+}
+
+// checker si une chaîne est un mot
+// source du code de base, modifié : https://codesource.io/building-a-word-counter-in-javascript/
+function isWord(str) {
+    var alphaNumericFound = false;
+    for (var i = 0; i < str.length; i++) {
+        var code = str.charCodeAt(i);
+        if ((code > 47 && code < 58) || // numeric (0-9)
+            (code > 64 && code < 91) || // upper alpha (A-Z)
+            (code > 96 && code < 123)) { // lower alpha (a-z)
+            alphaNumericFound = true;
+        return alphaNumericFound;
+        }
+    }
+    return alphaNumericFound;
+}
+
+function wordChecker(words) {
+    var words = inputText.text.split(' ');
+    var correctAnswer_array = correctAnswer.split(' ');
+    // console.log('checker est activé')
+
+    for (let i in words) {
+        if(words[i].toLowerCase() == correctAnswer_array[i].toLowerCase()){
+            inputText.setFontColor('green');
+        }
+        else {
+            inputText.setFontColor('red');
+        }
+      }
+}
+
+function reinitialiseInput() {
+    inputText.setText('')
 }
